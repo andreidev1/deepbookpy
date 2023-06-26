2915d9c8d5695d00dbe62f799cd555718bc36016
andreidev1 <d.andrei01@yahoo.com>
2023-06-26 15:58:08 +0300

    Change test_deepbook_query.py design

--- tests/integration/test_deepbook_query.py
+++ tests/integration/test_deepbook_query.py
@@ -1,19 +1,23 @@
 """Test Query DeepBook API"""
 from pysui.sui.sui_clients.sync_client import SuiClient
 from deepbookpy.deepbook_query import DeepBookQuery
+from tests.conftest import dee9_data
 
-   
+token_1 = dee9_data()['test']["token_1"]
+token_2 = dee9_data()["test"]["token_2"]
+pool_id = dee9_data()["test"]["pool_id"]
 
-def test_get_market_price(init_client, package_id):
-    """Test get market price query"""
+
+def test_get_market_price(init_client, dee9_package_id):
+    """Test market price query"""
 
     client = SuiClient(init_client)
-    deepbook_query = DeepBookQuery(client, package_id)
+    deepbook_query = DeepBookQuery(client, dee9_package_id)
 
     status = deepbook_query.get_market_price(
-    token_1="0x5378a0e7495723f7d942366a125a6556cf56f573fa2bb7171b554a2986c4229a::weth::WETH",
-    token_2="0x5378a0e7495723f7d942366a125a6556cf56f573fa2bb7171b554a2986c4229a::usdt::USDT",
-    pool_id="0xcaee8e1c046b58e55196105f1436a2337dcaa0c340a7a8c8baf65e4afb8823a4"
+    token_1=token_1,
+    token_2=token_2,
+    pool_id=pool_id
     ).__dict__['effects'].__dict__['status'].__dict__['status']
     
     assert status == 'success'
