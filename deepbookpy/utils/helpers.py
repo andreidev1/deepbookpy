ed1410a284ee3acc9be68d670a7694a9b3b08240
andreidev1 <d.andrei01@yahoo.com>
2023-08-07 20:55:42 +0300

    add order timestamp expiration

--- deepbookpy/utils/helpers.py
+++ deepbookpy/utils/helpers.py
@@ -1,5 +1,16 @@
-"""Parse response of asset type"""
+"""Helpers module with built-in features"""
+import time
+from deepbookpy.utils.constants import ORDER_DEFAULT_EXPIRATION_IN_MS
+
 
 def parse_struct(response):
-        response = response.replace('0xdee9::clob_v2::Pool<', '').replace(">", '').split(',')
-        return [el.strip() for el in response]
+    """Parse pool data -> base_asset_type and quote_asset_type"""
+    response = (
+        response.replace("0xdee9::clob_v2::Pool<", "").replace(">", "").split(",")
+    )
+    return [el.strip() for el in response]
+
+
+def order_24h_expiration():
+    """Get +24 hour UNIX timestamp"""
+    return int(time.time()) * 1000 + ORDER_DEFAULT_EXPIRATION_IN_MS
