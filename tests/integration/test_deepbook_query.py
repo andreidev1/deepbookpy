92dbdff38f7c97e5ca3110ee0ba3b11258540f49
andreidev1 <d.andrei01@yahoo.com>
2023-08-07 20:54:37 +0300

    black formatting

--- tests/integration/test_deepbook_query.py
+++ tests/integration/test_deepbook_query.py
@@ -5,8 +5,7 @@ from pysui.sui.sui_clients.sync_client import SuiClient
 from deepbookpy.deepbook_query import DeepBookQuery
 from tests.conftest import dee9_data_v2
 
-base_asset_type = dee9_data_v2()['test']["base_asset_type"]
-quote_asset_type = dee9_data_v2()["test"]["quote_asset_type"]
+
 pool_id = dee9_data_v2()["test"]["pool_id"]
 account_cap = dee9_data_v2()["test"]["account_cap"]
 
@@ -18,78 +17,81 @@ def test_get_market_price(init_client, dee9_package_id):
     client = SuiClient(init_client)
     deepbook_query = DeepBookQuery(client, dee9_package_id)
 
-    status = deepbook_query.get_market_price(
-    base_asset_type=base_asset_type,
-    quote_asset_type=quote_asset_type,
-    pool_id=pool_id
-    ).__dict__['effects'].__dict__['status'].__dict__['status']
-    
-    assert status == 'success'
+    status = (
+        deepbook_query.get_market_price(pool_id=pool_id)
+        .__dict__["effects"]
+        .__dict__["status"]
+        .__dict__["status"]
+    )
+
+    assert status == "success"
 
 
 @flaky.flaky()
 def test_get_user_position(init_client, dee9_package_id):
-
     client = SuiClient(init_client)
     deepbook_query = DeepBookQuery(client, dee9_package_id)
 
-    status = deepbook_query.get_user_position(
-    base_asset_type=base_asset_type,
-    quote_asset_type=quote_asset_type,
-    pool_id=pool_id,
-    account_cap=account_cap
-    ).__dict__['effects'].__dict__['status'].__dict__['status']
+    status = (
+        deepbook_query.get_user_position(pool_id=pool_id, account_cap=account_cap)
+        .__dict__["effects"]
+        .__dict__["status"]
+        .__dict__["status"]
+    )
 
-    assert status == 'success'
+    assert status == "success"
 
 
 @flaky.flaky()
 def test_list_open_orders(init_client, dee9_package_id):
-
     client = SuiClient(init_client)
     deepbook_query = DeepBookQuery(client, dee9_package_id)
 
-    status = deepbook_query.list_open_orders(
-    base_asset_type=base_asset_type,
-    quote_asset_type=quote_asset_type,
-    pool_id=pool_id,
-    account_cap=account_cap
-    ).__dict__['effects'].__dict__['status'].__dict__['status']
+    status = (
+        deepbook_query.list_open_orders(pool_id=pool_id, account_cap=account_cap)
+        .__dict__["effects"]
+        .__dict__["status"]
+        .__dict__["status"]
+    )
 
-    assert status == 'success'
+    assert status == "success"
 
 
 @flaky.flaky()
 def test_get_level2_book_status_true_bid(init_client, dee9_package_id):
-
     client = SuiClient(init_client)
     deepbook_query = DeepBookQuery(client, dee9_package_id)
 
-    status = deepbook_query.get_level2_book_status(
-    base_asset_type=base_asset_type,
-    quote_asset_type=quote_asset_type,
-    pool_id=pool_id,
-    lower_price=18000000000,
-    higher_price=20000000000,
-    is_bid_side=True
-    ).__dict__['effects'].__dict__['status'].__dict__['status']
+    status = (
+        deepbook_query.get_level2_book_status(
+            pool_id=pool_id,
+            lower_price=18000000000,
+            higher_price=20000000000,
+            is_bid_side=True,
+        )
+        .__dict__["effects"]
+        .__dict__["status"]
+        .__dict__["status"]
+    )
 
-    assert status == 'success'
+    assert status == "success"
 
 
 @flaky.flaky()
 def test_get_level2_book_status_false_bid(init_client, dee9_package_id):
-
     client = SuiClient(init_client)
     deepbook_query = DeepBookQuery(client, dee9_package_id)
 
-    status = deepbook_query.get_level2_book_status(
-    base_asset_type=base_asset_type,
-    quote_asset_type=quote_asset_type,
-    pool_id=pool_id,
-    lower_price=18000000000,
-    higher_price=20000000000,
-    is_bid_side=False
-    ).__dict__['effects'].__dict__['status'].__dict__['status']
+    status = (
+        deepbook_query.get_level2_book_status(
+            pool_id=pool_id,
+            lower_price=18000000000,
+            higher_price=20000000000,
+            is_bid_side=False,
+        )
+        .__dict__["effects"]
+        .__dict__["status"]
+        .__dict__["status"]
+    )
 
-    assert status == 'success'
+    assert status == "success"
