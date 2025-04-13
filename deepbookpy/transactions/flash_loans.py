bd256e9eaf3e01333771004cae45bc5c6e15f0dd
andrei <d.andrei01@yahoo.com>
2025-04-13 22:23:52 +0300

    fix flash_loans module

--- deepbookpy/transactions/flash_loans.py
+++ deepbookpy/transactions/flash_loans.py
@@ -1,5 +1,5 @@
 from pysui.sui.sui_txn.sync_transaction import SuiTransaction
-from pysui.sui.sui_types.scalars import ObjectID, SuiU64, SuiU8, SuiBoolean
+from pysui.sui.sui_types.scalars import ObjectID, SuiU64
 
 from deepbookpy.utils.config import DeepBookConfig
 
@@ -24,7 +24,7 @@ class FlashLoanContract:
         pool = self.__config.get_pool(pool_key)
         base_coin = self.__config.get_coin(pool['base_coin'])
         quote_coin = self.__config.get_coin(pool['quote_coin'])
-        input_quantity = round(borrow_amount * base_coin.scalar)
+        input_quantity = round(borrow_amount * base_coin["scalar"])
 
         base_coin_result, flash_loan = tx.move_call(
             target = f"{self.__config.DEEPBOOK_PACKAGE_ID}::pool::borrow_flashloan_base",
@@ -32,7 +32,7 @@ class FlashLoanContract:
             type_arguments=[base_coin['type'], quote_coin['type']],
         )
 
-        return base_coin_result, flash_loan
+        return [base_coin_result, flash_loan]
     
     def return_base_asset(self, pool_key: str, borrow_amount: int, base_coin_input: str, flash_loan: str, tx: SuiTransaction) -> SuiTransaction:
         """
@@ -47,10 +47,9 @@ class FlashLoanContract:
         pool = self.__config.get_pool(pool_key)
         base_coin = self.__config.get_coin(pool['base_coin'])
         quote_coin = self.__config.get_coin(pool['quote_coin'])
-        borrow_scalar = base_coin.scalar
+        borrow_scalar = base_coin["scalar"]
 
-        [base_coin_return] = tx.split_coin(coin=base_coin_input, amounts=[SuiU64(round(borrow_amount * borrow_scalar))])
-
+        base_coin_return = tx.split_coin(coin=base_coin_input, amounts=[round(borrow_amount * borrow_scalar)])
         tx.move_call(
             target = f"{self.__config.DEEPBOOK_PACKAGE_ID}::pool::return_flashloan_base",
             arguments=[ObjectID(pool['address']), base_coin_return, flash_loan],
@@ -71,7 +70,7 @@ class FlashLoanContract:
         pool = self.__config.get_pool(pool_key)
         base_coin = self.__config.get_coin(pool['base_coin'])
         quote_coin = self.__config.get_coin(pool['quote_coin'])
-        input_quantity = round(borrow_amount * quote_coin.scalar)
+        input_quantity = round(borrow_amount * quote_coin["scalar"])
 
         [quote_coin_result, flash_loan] = tx.move_call(
             target=f"{self.__config.DEEPBOOK_PACKAGE_ID}::pool::borrow_flashloan_quote",
@@ -82,7 +81,7 @@ class FlashLoanContract:
         return [quote_coin_result, flash_loan]
     
 
-    def return_quote_asset(self, pool_key:str,  borrow_amount: int, quote_coin_input: str, flash_loan: str, tx: SuiTransaction) -> SuiTransaction:
+    def return_quote_asset(self, pool_key:str, borrow_amount: int, quote_coin_input: str, flash_loan: str, tx: SuiTransaction) -> SuiTransaction:
         """
         Return quote asset to the pool after a flash loan.
 
@@ -96,11 +95,9 @@ class FlashLoanContract:
         pool = self.__config.get_pool(pool_key)
         base_coin = self.__config.get_coin(pool['base_coin'])
         quote_coin = self.__config.get_coin(pool['quote_coin'])
-        borrow_scalar = quote_coin.scalar
+        borrow_scalar = quote_coin["scalar"]
 
-        [quote_coin_return] = tx.split_coin(
-            amounts=[quote_coin_input, [SuiU64(round(borrow_amount * borrow_scalar))]]
-        )
+        quote_coin_return = tx.split_coin(coin=quote_coin_input, amounts=[round(borrow_amount * borrow_scalar)])
 
         tx.move_call(
             target=f"{self.__config.DEEPBOOK_PACKAGE_ID}::pool::return_flashloan_quote",
