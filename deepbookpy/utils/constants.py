0f87248ae5ef8ec822904122d08ce02d8f346e19
andrei <d.andrei01@yahoo.com>
2026-06-07 16:57:08 +0300

    update constants

--- deepbookpy/utils/constants.py
+++ deepbookpy/utils/constants.py
@@ -6,6 +6,7 @@ from deepbookpy.utils.normalizer import normalize_sui_object_id
 """Sui Constants"""
 
 CLOCK = normalize_sui_object_id("0x6")
+
 DEFAULT_EXPIRATION_TIMESTAMP = 1844674407370955161
 
 
@@ -14,40 +15,72 @@ class DeepbookPackageIds(TypedDict):
     DEEPBOOK_PACKAGE_ID: str
     REGISTRY_ID: str
     DEEP_TREASURY_ID: str
+    MARGIN_PACKAGE_ID: str
+    MARGIN_REGISTRY_ID: str
+    LIQUIDATION_PACKAGE_ID: str
 
 
 testnet_package_ids: DeepbookPackageIds = {
-    "DEEPBOOK_PACKAGE_ID": "0x984757fc7c0e6dd5f15c2c66e881dd6e5aca98b725f3dbd83c445e057ebb790a",
+    "DEEPBOOK_PACKAGE_ID": "0x22be4cade64bf2d02412c7e8d0e8beea2f78828b948118d46735315409371a3c",
     "REGISTRY_ID": "0x7c256edbda983a2cd6f946655f4bf3f00a41043993781f8674a7046e8c0e11d1",
     "DEEP_TREASURY_ID": "0x69fffdae0075f8f71f4fa793549c11079266910e8905169845af1f5d00e09dcb",
+    "MARGIN_PACKAGE_ID": "0xd6a42f4df4db73d68cbeb52be66698d2fe6a9464f45ad113ca52b0c6ebd918b6",
+    "MARGIN_REGISTRY_ID": "0x48d7640dfae2c6e9ceeada197a7a1643984b5a24c55a0c6c023dac77e0339f75",
+    "LIQUIDATION_PACKAGE_ID": "0x8d69c3ef3ef580e5bf87b933ce28de19a5d0323588d1a44b9c60b4001741aa24",
 }
 
 mainnet_package_ids: DeepbookPackageIds = {
-    "DEEPBOOK_PACKAGE_ID": "0xcaf6ba059d539a97646d47f0b9ddf843e138d215e2a12ca1f4585d386f7aec3a",
+    "DEEPBOOK_PACKAGE_ID": "0xf48222c4e057fa468baf136bff8e12504209d43850c5778f76159292a96f621e",
     "REGISTRY_ID": "0xaf16199a2dff736e9f07a845f23c5da6df6f756eddb631aed9d24a93efc4549d",
     "DEEP_TREASURY_ID": "0x032abf8948dda67a271bcc18e776dbbcfb0d58c8d288a700ff0d5521e57a1ffe",
+    "MARGIN_PACKAGE_ID": "0xfbd322126f1452fd4c89aedbaeb9fd0c44df9b5cedbe70d76bf80dc086031377",
+    "MARGIN_REGISTRY_ID": "0x0e40998b359a9ccbab22a98ed21bd4346abf19158bc7980c8291908086b3a742",
+    "LIQUIDATION_PACKAGE_ID": "0x55718c06706bee34c9f3c39f662f10be354a4dcc719699ad72091dc343b641b8",
 }
+
 
 testnet_coins: Dict[str, Coin] = {
     "DEEP": {
         "address": "0x36dbef866a1d62bf7328989a10fb2f07d769f4ee587c0de4a0a256e57e0a58a8",
         "type": "0x36dbef866a1d62bf7328989a10fb2f07d769f4ee587c0de4a0a256e57e0a58a8::deep::DEEP",
         "scalar": 1000000,
+        "feed": "0x99137a18354efa7fb6840889d059fdb04c46a6ce21be97ab60d9ad93e91ac758",  # DEEP uses HFT feed on testnet
+        "currency_id": "0xbf1b77e244f649c736a44898585cc8ac939fbb0bbdf1d8d2a183978cc312e613",
+        "price_info_object_id": "0x3d52fffa2cd9e54b39bb36d282bdda560b15b8b4fdf4766a3c58499ef172bafc",
     },
     "SUI": {
         "address": "0x0000000000000000000000000000000000000000000000000000000000000002",
         "type": "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
         "scalar": 1000000000,
+        "feed": "0x50c67b3fd225db8912a424dd4baed60ffdde625ed2feaaf283724f9608fea266",
+        "currency_id": "0xf256d3fb6a50eaa748d94335b34f2982fbc3b63ceec78cafaa29ebc9ebaf2bbc",
+        "price_info_object_id": "0x1ebb295c789cc42b3b2a1606482cd1c7124076a0f5676718501fda8c7fd075a0",
     },
     "DBUSDC": {
         "address": "0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7",
         "type": "0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7::DBUSDC::DBUSDC",
         "scalar": 1000000,
+        "feed": "0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722",
+        "currency_id": "0x509db0f9283c9ee4fdc5b99028a439d3639f49e9709e3d7a6de14b3bfdb0c784",
+        "price_info_object_id": "0x9c4dd4008297ffa5e480684b8100ec21cc934405ed9a25d4e4d7b6259aad9c81",
+    },
+    "DBTC": {
+        "address": "0x6502dae813dbe5e42643c119a6450a518481f03063febc7e20238e43b6ea9e86",
+        "type": "0x6502dae813dbe5e42643c119a6450a518481f03063febc7e20238e43b6ea9e86::dbtc::DBTC",
+        "scalar": 100000000,
+        "feed": "0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b",
+        "currency_id": "0x3ef2afa2126704bf721b9c8495d94288f6bd090fc454fe3e1613eb765a8a348f",
+        "price_info_object_id": "0x72431a238277695d3f31e4425225a4462674ee6cceeea9d66447b210755fffba",
     },
     "DBUSDT": {
         "address": "0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7",
         "type": "0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7::DBUSDT::DBUSDT",
         "scalar": 1000000,
+    },
+    "WAL": {
+        "address": "0x9ef7676a9f81937a52ae4b2af8d511a28a0b080477c0c2db40b0ab8882240d76",
+        "type": "0x9ef7676a9f81937a52ae4b2af8d511a28a0b080477c0c2db40b0ab8882240d76::wal::WAL",
+        "scalar": 1000000000,
     },
 }
 
@@ -56,16 +89,57 @@ mainnet_coins: Dict[str, Coin] = {
         "address": "0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270",
         "type": "0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270::deep::DEEP",
         "scalar": 1000000,
+        "feed": "0x29bdd5248234e33bd93d3b81100b5fa32eaa5997843847e2c2cb16d7c6d9f7ff",
+        "currency_id": "0x3f2afb7c5f245870a8b8a3808e6dd7042446a0e7504e9d2795372da053858cd9",
+        "price_info_object_id": "0x8c7f3a322b94cc69db2a2ac575cbd94bf5766113324c3a3eceac91e3e88a51ed",
     },
     "SUI": {
         "address": "0x0000000000000000000000000000000000000000000000000000000000000002",
         "type": "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
         "scalar": 1000000000,
+        "feed": "0x23d7315113f5b1d3ba7a83604c44b94d79f4fd69af77f804fc7f920a6dc65744",
+        "currency_id": "0xf256d3fb6a50eaa748d94335b34f2982fbc3b63ceec78cafaa29ebc9ebaf2bbc",
+        "price_info_object_id": "0x801dbc2f0053d34734814b2d6df491ce7807a725fe9a01ad74a07e9c51396c37",
     },
     "USDC": {
         "address": "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7",
         "type": "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
         "scalar": 1000000,
+        "feed": "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
+        "currency_id": "0x75cfbbf8c962d542e99a1d15731e6069f60a00db895407785b15d14f606f2b4a",
+        "price_info_object_id": "0x5dec622733a204ca27f5a90d8c2fad453cc6665186fd5dff13a83d0b6c9027ab",
+    },
+    "WAL": {
+        "address": "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59",
+        "type": "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59::wal::WAL",
+        "scalar": 1000000000,
+        "feed": "0xeba0732395fae9dec4bae12e52760b35fc1c5671e2da8b449c9af4efe5d54341",
+        "currency_id": "0xb6a0c0bacb1c87c3be4dff20c22ef1012125b5724b5b0ff424f852a2651b23fa",
+        "price_info_object_id": "0xeb7e669f74d976c0b99b6ef9801e3a77716a95f1a15754e0f1399ce3fb60973d",
+    },
+    "SUIUSDE": {
+        "address": "0x41d587e5336f1c86cad50d38a7136db99333bb9bda91cea4ba69115defeb1402",
+        "type": "0x41d587e5336f1c86cad50d38a7136db99333bb9bda91cea4ba69115defeb1402::sui_usde::SUI_USDE",
+        "scalar": 1000000,
+        "feed": "0x8cead549d0e770dea8fdf5e018a85d59585265cf8bff16ba83962fc7996dbb7f",
+        "currency_id": "0x44f0959110bd9e5e91af0483364c42075ac19f173b28f708989f419ef3560576",
+        "price_info_object_id": "0x9b2028bfc829127d2e5ead1691dc3002de9e9b8d8076b4915e5ecc7d9b99d63f",
+    },
+    "XBTC": {
+        "address": "0x876a4b7bce8aeaef60464c11f4026903e9afacab79b9b142686158aa86560b50",
+        "type": "0x876a4b7bce8aeaef60464c11f4026903e9afacab79b9b142686158aa86560b50::xbtc::XBTC",
+        "scalar": 100000000,
+        "feed": "0xae8f269ed9c4bed616c99a98cf6dfe562bd3202e7f91821a471ff854713851b4",
+        "currency_id": "0x907bb173bffab7c57bbd3350a633aa32c8770937b496d7d88874087b59200bcc",
+        "price_info_object_id": "0xa4b9db1866ee6e2a156e8c36fc66be0f68f232388ebb578c949c2c6beb50128b",
+    },
+    "USDSUI": {
+        "address": "0x44f838219cf67b058f3b37907b655f226153c18e33dfcd0da559a844fea9b1c1",
+        "type": "0x44f838219cf67b058f3b37907b655f226153c18e33dfcd0da559a844fea9b1c1::usdsui::USDSUI",
+        "scalar": 1000000,
+        "feed": "0xd510fcdb3a63f35d3bb118d5db3afc5815a3f13bc55d48abb893b63f0315902a",
+        "currency_id": "0x535e826a2acddab687c81cb6c6166553b479f61a9023800ec0020baba8d94731",
+        "price_info_object_id": "0x68644a3ab7a1aab113a4a68b6115a5b51eba4cb6aaac2d99b734be2e5e748425",
     },
     "WUSDC": {
         "address": "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf",
@@ -112,7 +186,33 @@ mainnet_coins: Dict[str, Coin] = {
         "type": "0x294de7579d55c110a00a7c4946e09a1b5cbeca2592fbb83fd7bfacba3cfeaf0e::drf::DRF",
         "scalar": 1000000,
     },
+    "SEND": {
+        "address": "0xb45fcfcc2cc07ce0702cc2d229621e046c906ef14d9b25e8e4d25f6e8763fef7",
+        "type": "0xb45fcfcc2cc07ce0702cc2d229621e046c906ef14d9b25e8e4d25f6e8763fef7::send::SEND",
+        "scalar": 1000000,
+    },
+    "IKA": {
+        "address": "0x7262fb2f7a3a14c888c438a3cd9b912469a58cf60f367352c46584262e8299aa",
+        "type": "0x7262fb2f7a3a14c888c438a3cd9b912469a58cf60f367352c46584262e8299aa::ika::IKA",
+        "scalar": 1000000000,
+    },
+    "ALKIMI": {
+        "address": "0x1a8f4bc33f8ef7fbc851f156857aa65d397a6a6fd27a7ac2ca717b51f2fd9489",
+        "type": "0x1a8f4bc33f8ef7fbc851f156857aa65d397a6a6fd27a7ac2ca717b51f2fd9489::alkimi::ALKIMI",
+        "scalar": 1000000000,
+    },
+    "LZWBTC": {
+        "address": "0x0041f9f9344cac094454cd574e333c4fdb132d7bcc9379bcd4aab485b2a63942",
+        "type": "0x0041f9f9344cac094454cd574e333c4fdb132d7bcc9379bcd4aab485b2a63942::wbtc::WBTC",
+        "scalar": 100000000,
+    },
+    "USDT": {
+        "address": "0x375f70cf2ae4c00bf37117d0c85a2c71545e6ee05c4a5c7d282cd66a4504b068",
+        "type": "0x375f70cf2ae4c00bf37117d0c85a2c71545e6ee05c4a5c7d282cd66a4504b068::usdt::USDT",
+        "scalar": 1000000,
+    },
 }
+
 
 testnet_pools = {
     "DEEP_SUI": {
@@ -133,6 +233,21 @@ testnet_pools = {
     "DBUSDT_DBUSDC": {
         "address": "0x83970bb02e3636efdff8c141ab06af5e3c9a22e2f74d7f02a9c3430d0d10c1ca",
         "base_coin": "DBUSDT",
+        "quote_coin": "DBUSDC",
+    },
+    "WAL_DBUSDC": {
+        "address": "0xeb524b6aea0ec4b494878582e0b78924208339d360b62aec4a8ecd4031520dbb",
+        "base_coin": "WAL",
+        "quote_coin": "DBUSDC",
+    },
+    "WAL_SUI": {
+        "address": "0x8c1c1b186c4fddab1ebd53e0895a36c1d1b3b9a77cd34e607bef49a38af0150a",
+        "base_coin": "WAL",
+        "quote_coin": "SUI",
+    },
+    "DBTC_DBUSDC": {
+        "address": "0x0dce0aa771074eb83d1f4a29d48be8248d4d2190976a5241f66b43ec18fa34de",
+        "base_coin": "DBTC",
         "quote_coin": "DBUSDC",
     },
 }
@@ -198,4 +313,126 @@ mainnet_pools = {
         "base_coin": "DRF",
         "quote_coin": "SUI",
     },
+    "SEND_USDC": {
+        "address": "0x1fe7b99c28ded39774f37327b509d58e2be7fff94899c06d22b407496a6fa990",
+        "base_coin": "SEND",
+        "quote_coin": "USDC",
+    },
+    "WAL_USDC": {
+        "address": "0x56a1c985c1f1123181d6b881714793689321ba24301b3585eec427436eb1c76d",
+        "base_coin": "WAL",
+        "quote_coin": "USDC",
+    },
+    "WAL_SUI": {
+        "address": "0x81f5339934c83ea19dd6bcc75c52e83509629a5f71d3257428c2ce47cc94d08b",
+        "base_coin": "WAL",
+        "quote_coin": "SUI",
+    },
+    "XBTC_USDC": {
+        "address": "0x20b9a3ec7a02d4f344aa1ebc5774b7b0ccafa9a5d76230662fdc0300bb215307",
+        "base_coin": "XBTC",
+        "quote_coin": "USDC",
+    },
+    "IKA_USDC": {
+        "address": "0xfa732993af2b60d04d7049511f801e79426b2b6a5103e22769c0cead982b0f47",
+        "base_coin": "IKA",
+        "quote_coin": "USDC",
+    },
+    "ALKIMI_SUI": {
+        "address": "0x84752993c6dc6fce70e25ddeb4daddb6592d6b9b0912a0a91c07cfff5a721d89",
+        "base_coin": "ALKIMI",
+        "quote_coin": "SUI",
+    },
+    "LZWBTC_USDC": {
+        "address": "0xf5142aafa24866107df628bf92d0358c7da6acc46c2f10951690fd2b8570f117",
+        "base_coin": "LZWBTC",
+        "quote_coin": "USDC",
+    },
+    "USDT_USDC": {
+        "address": "0xfc28a2fb22579c16d672a1152039cbf671e5f4b9f103feddff4ea06ef3c2bc25",
+        "base_coin": "USDT",
+        "quote_coin": "USDC",
+    },
+    "SUIUSDE_USDC": {
+        "address": "0x0fac1cebf35bde899cd9ecdd4371e0e33f44ba83b8a2902d69186646afa3a94b",
+        "base_coin": "SUIUSDE",
+        "quote_coin": "USDC",
+    },
+    "SUI_SUIUSDE": {
+        "address": "0x034f3a42e7348de2084406db7a725f9d9d132a56c68324713e6e623601fb4fd7",
+        "base_coin": "SUI",
+        "quote_coin": "SUIUSDE",
+    },
+    "SUI_USDSUI": {
+        "address": "0x826eeacb2799726334aa580396338891205a41cf9344655e526aae6ddd5dc03f",
+        "base_coin": "SUI",
+        "quote_coin": "USDSUI",
+    },
+    "USDSUI_USDC": {
+        "address": "0xa374264d43e6baa5aa8b35ff18ff24fdba7443b4bcb884cb4c2f568d32cdac36",
+        "base_coin": "USDSUI",
+        "quote_coin": "USDC",
+    },
+}
+
+
+testnet_margin_pools = {
+    "SUI": {
+        "address": "0xcdbbe6a72e639b647296788e2e4b1cac5cea4246028ba388ba1332ff9a382eea",
+        "type": "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
+    },
+    "DBUSDC": {
+        "address": "0xf08568da93834e1ee04f09902ac7b1e78d3fdf113ab4d2106c7265e95318b14d",
+        "type": "0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7::DBUSDC::DBUSDC",
+    },
+    "DEEP": {
+        "address": "0x610640613f21d9e688d6f8103d17df22315c32e0c80590ce64951a1991378b55",
+        "type": "0x36dbef866a1d62bf7328989a10fb2f07d769f4ee587c0de4a0a256e57e0a58a8::deep::DEEP",
+    },
+    "DBTC": {
+        "address": "0xf3440b4aafcc8b12fc4b242e9590c52873b8238a0d0e52fbf9dae61d2970796a",
+        "type": "0x6502dae813dbe5e42643c119a6450a518481f03063febc7e20238e43b6ea9e86::dbtc::DBTC",
+    },
+}
+
+mainnet_margin_pools = {
+    "SUI": {
+        "address": "0x53041c6f86c4782aabbfc1d4fe234a6d37160310c7ee740c915f0a01b7127344",
+        "type": "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
+    },
+    "USDC": {
+        "address": "0xba473d9ae278f10af75c50a8fa341e9c6a1c087dc91a3f23e8048baf67d0754f",
+        "type": "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
+    },
+    "DEEP": {
+        "address": "0x1d723c5cd113296868b55208f2ab5a905184950dd59c48eb7345607d6b5e6af7",
+        "type": "0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270::deep::DEEP",
+    },
+    "WAL": {
+        "address": "0x38decd3dbb62bd4723144349bf57bc403b393aee86a51596846a824a1e0c2c01",
+        "type": "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59::wal::WAL",
+    },
+    "SUIUSDE": {
+        "address": "0xbb990ca04a7743e6c0a25a7fb16f60fc6f6d8bf213624ff03a63f1bb04c3a12f",
+        "type": "0x41d587e5336f1c86cad50d38a7136db99333bb9bda91cea4ba69115defeb1402::sui_usde::SUI_USDE",
+    },
+    "XBTC": {
+        "address": "0x14dfbf54400e0b97e892349310d392bef6d187c2b6709d9b246b8f41c9a13de4",
+        "type": "0x876a4b7bce8aeaef60464c11f4026903e9afacab79b9b142686158aa86560b50::xbtc::XBTC",
+    },
+    "USDSUI": {
+        "address": "0x78a0ddd02745d9b500fb7e9aae2ff8b665d974f00fd1f6060d59f4a8e891402c",
+        "type": "0x44f838219cf67b058f3b37907b655f226153c18e33dfcd0da559a844fea9b1c1::usdsui::USDSUI",
+    },
+}
+
+
+testnet_pyth_configs = {
+    "pyth_state_id": "0x243759059f4c3111179da5878c12f68d612c21a8d54d85edc86164bb18be1c7c",
+    "wormhole_state_id": "0x31358d198147da50db32eda2562951d53973a0c0ad5ed738e9b17d88b213d790",
+}
+
+mainnet_pyth_configs = {
+    "pyth_state_id": "0x1f9310238ee9298fb703c3419030b35b22bb1cc37113e3bb5007c99aec79e5b8",
+    "wormhole_state_id": "0xaeab97f96cf9877fee2883315d459552b2b921edc16d7ceac6eab944dd88919c",
 }
